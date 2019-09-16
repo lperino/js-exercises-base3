7c07734e87a59db14a2590a5e0d4a38a8c2cc0ce
léni <lenipep@hotmail.fr>
2019-09-16 11:25:04 +0200

    init 2

---
+++ exercice 1/script1_3.js
@@ -0,0 +1,8 @@
+var bordure = document.getElementById("image1")
+
+bordure.addEventListener("mouseover",function(){
+    this.style.border= "black solid 5px"   }
+)   
+bordure.addEventListener("mouseleave",function(){
+    this.style.border="none"
+})
